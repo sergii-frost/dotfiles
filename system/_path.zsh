5cb9892c9938197f657d4d9cb0a4cfef2f311dc0
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2014-10-13 11:15:11 +0200

    Adding Z-zsh; updating path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,11 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+#Setting additional env variables
+export DEV_HOME="${HOME}/Developer"
+export DEV_TOOLS="${DEV_HOME}/TOOLS"
+export XCTOOL_HOME="${DEV_TOOLS}/xctool"
+
+#Adding Android SDK
+export ANDROID_HOME="/Applications/Android Studio.app/sdk"
+export PATH=$PATH:${ANDROID_HOME}/platform-tools
+export PATH=$PATH:${ANDROID_HOME}/tools
