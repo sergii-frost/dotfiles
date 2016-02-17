c62a34ee31d98511cca0b7e54fa15dea8c5034f5
Sergii Nezdolii <sergii@Sergiis-MacBook-Pro.local>
2016-02-17 13:20:42 +0100

    Merging

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,9 +12,11 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gci='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gbrowse='git browse'
